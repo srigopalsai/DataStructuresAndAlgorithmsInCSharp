b951c7fa09948a819725b9e535dc37e9626440b7
Srigopal Chitrapu <srchit@microsoft.com>
2017-05-10 22:59:12 -0700

    Testing the commit.

--- 0.0 - Assembly Language/Notes.cs
+++ 0.0 - Assembly Language/Notes.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace DataStructuresAndAlgorithms._0._0___Assembly_Language
+namespace DataStructuresAndAlgorithms
 {
     /*
 
